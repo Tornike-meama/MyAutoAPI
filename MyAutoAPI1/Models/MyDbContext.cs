67545bdfdcd42308cd8c8e35b7966a894afc5119
Dric77 <92301957+Tornike-meama@users.noreply.github.com>
2022-07-29 22:51:44 +0400

    Update MyDbContext.cs

--- MyAutoAPI1/Models/MyDbContext.cs
+++ MyAutoAPI1/Models/MyDbContext.cs
@@ -13,6 +13,5 @@ namespace MyAutoAPI1.Models
 
         public DbSet<Statement> Statement { get; set; }
         public DbSet<Currency> Currencies { get; set; }
-        public object FirstOrDefaault { get; internal set; }
     }
 }
